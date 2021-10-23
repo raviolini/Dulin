9ee8093fe5158925e05ac9eb283a0541e361f40f
cowdingus <zaki.nadhif45@gmail.com>
2021-10-23 17:11:33 +0700

    Get rid of absolute path in run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-JAVA_HOME="/usr/lib/jvm/default" cd /home/zndf/Documents/Programming_Playground/Java/LWJGL_Project; ./gradlew --configure-on-demand -x check run
+JAVA_HOME="/usr/lib/jvm/default" ./gradlew --configure-on-demand -x check run
