94d9b2d9f0efd2ccf8c24e05413e0fa9f3c679a1
cowdingus <zaki.nadhif45@gmail.com>
2021-10-24 20:34:56 +0700

    Write ShaderCompilationException

---
+++ src/main/java/LWJGL_Project/Exceptions/ShaderCompilationException.java
@@ -0,0 +1,30 @@
+package LWJGL_Project.Exceptions;
+
+import java.lang.Exception;
+import java.nio.IntBuffer;
+import java.nio.file.Path;
+
+import org.lwjgl.BufferUtils;
+
+import static org.lwjgl.opengl.GL33.*;
+
+public class ShaderCompilationException extends Exception {
+  public ShaderCompilationException(Path shaderPath, int shaderHandle) {
+    super(shaderPath.toString() + " (" + getShaderType(shaderHandle) + "):\n" + glGetShaderInfoLog(shaderHandle));
+  }
+
+  private static String getShaderType(int shaderHandle) {
+    IntBuffer shaderType = BufferUtils.createIntBuffer(1);
+    glGetShaderiv(shaderHandle, GL_SHADER_TYPE, shaderType);
+
+    if (shaderType.get(0) == GL_VERTEX_SHADER) {
+      return "GL_VERTEX_SHADER";
+    } else if (shaderType.get(0) == GL_FRAGMENT_SHADER) {
+      return "GL_FRAGMENT_SHADER";
+    } else if (shaderType.get(0) == GL_GEOMETRY_SHADER) {
+      return "GL_GEOMETRY_SHADER";
+    } else {
+      return "UNKNOWN_SHADER";
+    }
+  }
+}
